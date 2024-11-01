726a0146f891ccc19bd878ce89d51fea8ae8ff79
parv nema <115030244+PARVNEMA@users.noreply.github.com>
2024-11-01 14:37:54 +0530

    front page

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,40 @@
-import { Button } from "@/components/ui/button";
-import { UserButton } from "@clerk/nextjs";
-import { LifeBuoyIcon } from "lucide-react";
-import Image from "next/image";
+"use client";
+import React from "react";
+import { cn } from "@/lib/utils";
+import { Spotlight } from "../components/ui/spotlight";
+import { ShootingStars } from "@/components/ui/shooting-stars";
+import { Meteors } from "@/components/ui/meteors";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 export default function Home() {
 	return (
-		<div className="flex h-screen flex-col items-center justify-center">
-			<h2>
-				Naman Nema <LifeBuoyIcon />
-			</h2>
-			<Button>Khana Khalo</Button>
-			<Button className="bg-secondary text-black">
-				Khana Khalo
-			</Button>
-			<Button className="bg-accent text-black">
-				Khana Khalo
-			</Button>
-			<UserButton />
+		<div className="h-[40rem] w-full rounded-md flex md:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
+			<Spotlight
+				className="-top-40 left-0 md:left-60 md:-top-20"
+				fill="white"
+			/>
+			<div className=" p-4 max-w-7xl  mx-auto relative z-10  w-full pt-20 md:pt-0">
+				<h1 className="text-4xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
+					All Your AI <br /> In One.
+				</h1>
+				<p className="mt-4 font-normal text-base text-neutral-300 max-w-lg text-center mx-auto">
+					Spotlight effect is a great way to draw attention
+					to a specific part of the page. Here, we are
+					drawing the attention towards the text section of
+					the page. I don&apos;t know why but I&apos;m
+					running out of copy.
+					<Link href={"/dashboard"}>
+						<button className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 mt-8">
+							<span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+							<span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+								Get Started
+							</span>
+						</button>
+					</Link>
+				</p>
+			</div>
+			<Meteors number={30} />
 		</div>
 	);
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -65,6 +65,33 @@ const config: Config = {
 				"4xl": "2.441rem",
 				"5xl": "3.052rem",
 			},
+			animation: {
+				spotlight: "spotlight 2s ease .75s 1 forwards",
+				"meteor-effect": "meteor 5s linear infinite",
+			},
+			keyframes: {
+				spotlight: {
+					"0%": {
+						opacity: "0",
+						transform: "translate(-72%, -62%) scale(0.5)",
+					},
+					"100%": {
+						opacity: "1",
+						transform: "translate(-50%,-40%) scale(1)",
+					},
+				},
+				meteor: {
+					"0%": {
+						transform: "rotate(215deg) translateX(0)",
+						opacity: "1",
+					},
+					"70%": { opacity: "1" },
+					"100%": {
+						transform: "rotate(215deg) translateX(-500px)",
+						opacity: "0",
+					},
+				},
+			},
 		},
 	},
 	plugins: [require("tailwindcss-animate")],
